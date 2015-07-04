590d91b43e64aeac35c8f00ae4354a8a36d2653a
Darren Tsung <darren.tsung@gmail.com>
2015-07-04 10:56:50 -0700

    Trying to get pixel perfect sprites, but hard D:

--- EpicGameJam2/Assets/Scripts/Player/PlayerMovementController.cs
+++ EpicGameJam2/Assets/Scripts/Player/PlayerMovementController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class PlayerMovementController : MonoBehaviour {
 	public const string HORIZONTAL_AXIS_KEY = "Horizontal";
+	public const float PIXEL_SIZE = 0.1f;
 	
 	protected Rigidbody2D _rigidbody;
 	protected Animator _animator;
+	protected Transform _spriteTransforms;
 	protected DT.TweakableFloat _playerSpeed;
 	protected float PlayerSpeed {
 		get { return _playerSpeed.Value; }
@@ -14,6 +16,7 @@ public class PlayerMovementController : MonoBehaviour {
 	
 	public void Awake() {
 		_playerSpeed = new DT.TweakableFloat("PlayerSpeed", 0.0f, 10.0f, 2.0f);
+		_spriteTransforms = transform.Find("Sprites");
 		_rigidbody = GetComponent<Rigidbody2D>();
 		_animator = GetComponent<Animator>();
 	}
@@ -22,6 +25,13 @@ public class PlayerMovementController : MonoBehaviour {
 		currentXAxis = Input.GetAxis(HORIZONTAL_AXIS_KEY);
 		
 		_animator.speed = currentXAxis;
+	}
+	
+	public void LateUpdate() {
+		// offset the sprite from the player so that they are pixel perfect
+		float offsetX = -(transform.position.x % PIXEL_SIZE);
+		float offsetY = -(transform.position.y % PIXEL_SIZE);
+		_spriteTransforms.localPosition = new Vector3(offsetX, offsetY);
 	}
 	
 	public void FixedUpdate() {
