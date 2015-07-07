6644986d20d2b44078ed0a11185b84dca4f867e1
Darren Tsung <darren.tsung@gmail.com>
2015-07-07 09:20:43 -0700

    Whoops forgot to commit this..

---
+++ EpicGameJam2/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour, IGameStateInterface {
+	// PRAGMA MARK - IGameStateInterface
+	void IGameStateInterface.Reset() {
+		transform.position = _storedStartPosition;
+	}
+	
+	protected Vector3 _storedStartPosition;
+	
+	protected void Start () {
+		_storedStartPosition = transform.position;
+	}
+}
